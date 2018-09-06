47cf645f7d5ebfab76766d57826339cd14ba10b9
Merline Belizaire <merline@noukod.com>
2018-09-06 14:09:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,16 @@
 class Dog
-def initialize=(name)
+  def initialize(name)
   @name = name
+  end
 
+  def name=(name)
+  @name = name
+  end
+
+  def name
+  @name
+  end
 end
-end
+
+fido = Dog.new("Fido", "Pug")
+fido.name
